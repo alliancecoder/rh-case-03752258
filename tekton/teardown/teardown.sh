43dbdfbc15967be16fb3dccdfa329a71cadad7be
Stephen W. Boyd <sboyd@electricaltrainingalliance.org>
2024-09-13 09:47:01 -0400

    updated 2024-09-13

--- tekton/teardown/teardown.sh
+++ tekton/teardown/teardown.sh
@@ -11,6 +11,6 @@ oc delete pvc maven-local-repo-pvc
 oc delete pvc pipeline-task-cache-pvc
 oc delete rolebinding case03752258-pipeline-sa-scc-rolebinding
 oc delete clusterrolebinding case03752258-pipelines-as-code-cleanup-job
-oc delete clusterrole pipelines-as-code-cleanup-job
+# oc delete clusterrole pipelines-as-code-cleanup-job
 oc delete sa case03752258-pipeline-sa
 oc delete project case03752258
